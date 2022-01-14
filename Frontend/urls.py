4180e2db004bfdd82200fe1013a3add1710d7975
SpastBanaan <87088356+SpastBanana@users.noreply.github.com>
2022-01-14 22:04:03 +0100

    update

--- Frontend/urls.py
+++ Frontend/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.homeView, name="home"),
     path('login', views.loginView, name="home"),
     path('logout', views.logoutView, name="Logout"),
+    path('tarieven', views.tarievenView, name="Tarieven"),
+    path('overMij', views.overMijView, name="OverMij"),
 ]
--- Frontend/views.py
+++ Frontend/views.py
@@ -3,14 +3,33 @@ from django.contrib.auth import login, logout, authenticate
 from Sitemanager.models import mainPrice, salePrices, openedTimes
 
 def homeView(request):
-    knippie = mainPrice.objects.last()
-    kortie = salePrices.objects.all()
     opened = openedTimes.objects.all()
 
     data = {
         'page': 'home.html',
-        'mainPrice': knippie,
-        'kortingen': kortie,
+        'opened': opened,
+    }
+
+    return render(request, 'index.html', data)
+
+
+def tarievenView(request):
+
+    data = {
+        'page': 'tarieven.html',
+        'opened': openedTimes.objects.all(),
+        'sales': salePrices.objects.all(),
+        'price': mainPrice.objects.last(),
+    }
+
+    return render(request, 'index.html', data)
+
+
+def overMijView(request):
+    opened = openedTimes.objects.all()
+
+    data = {
+        'page': 'overMij.html',
         'opened': opened,
     }
 
@@ -18,6 +37,8 @@ def homeView(request):
 
 
 def loginView(request):
+    opened = openedTimes.objects.all()
+
     if request.method == 'POST' and 'login' in request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -30,18 +51,21 @@ def loginView(request):
                 data = {
                     'page': 'login.html',
                     'error': 'User account not activated',
+                    'opened': opened,
                 }
                 return render(request, 'index.html', data)
         else:
             data = {
                 'page': 'login.html',
                 'error': 'Incorrect password and/or username',
+                'opened': opened,
             }
             return render(request, 'index.html', data)
 
     data = {
         'page': 'login.html',
         'error': '',
+        'opened': opened,
     }
 
     return render(request, 'index.html', data)
